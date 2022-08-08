f6ecb588e9d69a7ed74a66b8ba5ab1890fb3d52e
kravchenkoegorii <kravchenko13138@gmail.com>
2022-08-08 11:24:07 +0300

    added process data

---
+++ HospitalService.MessageConsumer/ServiceBusMessaging/IProcessData.cs
@@ -0,0 +1,7 @@
+﻿namespace HospitalService.MessageConsumer.ServiceBusMessaging
+{
+    public interface IProcessData
+    {
+        Task Process(string value);
+    }
+}
---
+++ HospitalService.MessageConsumer/ServiceBusMessaging/ProcessData.cs
@@ -0,0 +1,22 @@
+﻿using HospitalService.MessageConsumer.Data;
+using HospitalService.MessageConsumer.Models;
+
+namespace HospitalService.MessageConsumer.ServiceBusMessaging
+{
+    public class ProcessData : IProcessData
+    {
+        private readonly string _connectionString = Environment.GetEnvironmentVariable("MESSAGEDB_KEY");
+
+        public ProcessData() { }
+
+        public async Task Process(string value)
+        {
+            using (var payloadMessageContext = new PayloadMessageContext(_connectionString))
+            {
+                await payloadMessageContext.AddAsync(new Message(value));
+                await payloadMessageContext.SaveChangesAsync();
+            }
+        }
+
+    }
+}
